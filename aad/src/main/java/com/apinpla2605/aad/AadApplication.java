0b9929ac8ad27316f761fb4a629d94d5ab5a48f2
Albertito98 <apinpla2605@g.educaand.es>
2025-10-23 18:06:41 +0200

    Master

--- aad/src/main/java/com/apinpla2605/aad/AadApplication.java
+++ aad/src/main/java/com/apinpla2605/aad/AadApplication.java
@@ -1,21 +1,23 @@
-package com.apinpla2605.aad;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import lombok.extern.slf4j.Slf4j;
-
-@SpringBootApplication
-@Slf4j
-
-public class AadApplication implements CommandLineRunner {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AadApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("lombok on");
-	}
-}
+//package com.apinpla2605.aad;
+//
+//import com.apinpla2605.aad.Repository.StudentRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@SpringBootApplication
+//@Slf4j
+//
+//public class AadApplication implements CommandLineRunner {
+//
+//	private final StudentRepository studentRepository;
+//	public static void main(String[] args) {
+//		SpringApplication.run(AadApplication.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+//	}
+//}
